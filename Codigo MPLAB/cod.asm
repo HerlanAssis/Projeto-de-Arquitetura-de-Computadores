2906bc4a5edaafd68b54b867863fa46ac02fe020
herlanassis <herlanassis@gmail.com>
2016-03-31 18:37:28 -0300

    Codigo comentado

--- Codigo MPLAB/cod.asm
+++ Codigo MPLAB/cod.asm
@@ -4,7 +4,6 @@
 			
 			;Espaço das variáveis
 			num_acc equ	  0x20 ;pode declarar variáveis em 0x21, 0x22 ...
-			delayl  equ   0x30
 ;----------------------------------------------------------------------------------------------
 ;Não editar esta seção
 ;----------------------------------------------------------------------------------------------			
@@ -32,47 +31,46 @@ setports    clrw                    ; Zero em W.
 		    clrf num_acc ;zera valor do contador
 ;Estado Inicial			
 inicial		
-			bsf		PORTA,0	;Liga apenas o led vermelho
-			bcf		PORTA,1	;Desliga apenas o led verde
+			bsf		PORTA,0	;Liga apenas o led vermelho / Endereço igual a 1 lógico
+			bcf		PORTA,1	;Desliga apenas o led verde / Endereço igual a 0 lógico
 
-			btfsc	PORTB,5 ;Verifica se a chave 'sensor' foi ligada
+			btfsc	PORTB,5 ;Verifica se a chave 'sensor' foi ligada / se for 0 lógico ele pula uma instução
 			call	passar  ;Se foi pressionado, vá para o estado passar
-			btfsc   PORTB,6 ;Verifica se a chave 'manutencao' foi ligada						
-			call    manutencao
+			btfsc   PORTB,6 ;Verifica se a chave 'manutencao' foi ligado / se for 1 lógico ele pula uma instução
+			call    manutencao ; chama a função de manutenção
 
-			goto	inicial
+			goto	inicial ; retorna para inicial
 			
 
-passar		bcf		PORTA,0	;Desliga apenas o led vermelho (conectado ao RA0)
-			bsf		PORTA,1	;Liga apenas o led verde (conectado ao RA1)
+passar		bcf		PORTA,0	;Desliga apenas o led vermelho (conectado ao RA0) / seta 0 lógico
+			bsf		PORTA,1	;Liga apenas o led verde (conectado ao RA1) / seta 1 lógico
 			incf    num_acc ;incrementa 1 sempre que o botão é ligado
 
 passar_loop			
-			btfss	PORTB,5 ;checa se o botão do 'sensor' permanece ligado
-			return			;se não permanece, retorne a subrotina de origem			
+			btfss	PORTB,5 ;checa se o botão do 'sensor' permanece ligado / verifica se o valor lógico é 1 caso seja pula uma linha
+			return			;se não permanece, retorne a subrotina de origem
 
-			goto	passar_loop
+			goto	passar_loop ; retorna para passar_loop
 
 
-manutencao  bcf   PORTA,0; Desliga led vermelho
-			bcf   PORTA,1; Desliga led verde
-			call cmp
+manutencao  bcf   PORTA,0; Desliga led vermelho / seta o valor lógico 0
+			bcf   PORTA,1; Desliga led verde / seta o valor lógico 0
+			call cmp ; chama a função de comparação de números
 
 manutencao_loop
 			btfss PORTB,6; Checa se o botão do 'manutenção' permanece ligado
-			return; Verificação da variavel 0x20
+			return; retorna para a subrotina
 
-			goto manutencao_loop
+			goto manutencao_loop ; retorna para manutenção_loop
 
-apagaLed	bcf   PORTA,0;liga led vermelho
-			bcf   PORTA,1;liga led verde
-			btfss PORTB,6
-			goto  inicial
+apagaLed	bcf   PORTA,0; liga led vermelho
+			bcf   PORTA,1; liga led verde
+			btfss PORTB,6; verifica se o valor lógico é 1 caso seja pula uma linha
+			goto  inicial ; retorna para inicial
 				
-
 apagaLed_loop
-			btfss PORTB,6;checa se o botão do 'manutenção' permanece ligado
-			return; Verificação da variavel 0x20
+			btfss PORTB,6; checa se o botão do 'manutenção' permanece ligado
+			return; retorna para a subrotina
 			
 			goto apagaLed_loop
 
@@ -81,12 +79,9 @@ acendeLed	bsf   PORTA,0;liga led vermelho
 
 acendeLed_loop
 			btfss PORTB,6; Checa se o botão do 'manutenção' permanece ligado
-			call apagaLed;verificação da variavel 0x20
+			call apagaLed; chama a função apagaLed
 		
-			goto acendeLed_loop
-
-
-
+			goto acendeLed_loop ; retorna para acender_loop
 
 ;---------------------------------------------------------------------------------------------			
 ;Função para a comparação da magnitude de dois números
@@ -107,11 +102,11 @@ cmp
    			;se chegar aqui 'num_acc' é menor que 10
    			;retlw 0
 menor_igual
-			call apagaLed
+			call apagaLed ; goto
    			retlw 0 ;retorna da função com o w_reg com valor 0
 
 maior
-			call acendeLed
+			call acendeLed ; goto
    			retlw 1 ;retorna da função com o w_reg com valor 1
 
 			
